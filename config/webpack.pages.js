8275b44037d6218d547025e33742d4961a41be25
Anna Matveeva <swallowtailnetae@gmail.com>
2025-12-08 00:25:24 +0300

    styleguide

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -1,16 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-function createPages(template, filename) {
+function createPages(template, filename, chunks) {
   return new HtmlWebpackPlugin({
     template: template,
-    filename: filename
-  })
+    filename: filename,
+    chunks: chunks,
+  });
 }
 
 const htmlPages = [
-  createPages('./src/index.html', './index.html'),
-  createPages('./src/pages/about.html', './pages/about.html'),
-  createPages('./src/pages/splash.html', './pages/splash.html')
-]
+  createPages("./src/index.html", "./index.html", ["index"]),
+  createPages("./src/pages/about.html", "./pages/about.html", ["index"]),
+  createPages("./src/pages/splash.html", "./pages/splash.html", ["index"]),
+  createPages("./src/styleguide.html", "./styleguide.html", ["styleguide"]),
+];
 
 module.exports = htmlPages
