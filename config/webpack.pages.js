8fc2e45ed67ffac405b4996e1c9f3e49a08b081c
meenshery <meenshery@gmail.com>
2026-03-23 15:46:09 +0300

    разводящая на интерактивы

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -12,45 +12,22 @@ const htmlPages = [
   createPages("./src/index.html", "./index.html", ["index"]),
   createPages("./src/pages/about.html", "./pages/about.html", ["index"]),
   createPages("./src/pages/splash.html", "./pages/splash.html", ["index"]),
-  createPages("./src/pages/articles.html", "./pages/articles.html", [
-    "articles",
-  ]),
+  createPages("./src/pages/articles.html", "./pages/articles.html", ["articles"]),
   createPages("./src/pages/random.html", "./pages/random.html", ["index"]),
-  createPages("./src/pages/constructor.html", "./pages/constructor.html", [
-    "index",
-  ]),
-  createPages(
-    "./src/pages/articles/art-1.html",
-    "./pages/articles/art-1.html",
-    ["articles"],
-  ),
-  createPages(
-    "./src/pages/articles/art-2.html",
-    "./pages/articles/art-2.html",
-    ["articles"],
-  ),
-  createPages(
-    "./src/pages/articles/art-3.html",
-    "./pages/articles/art-3.html",
-    ["articles"],
-  ),
-  createPages(
-    "./src/pages/articles/art-4.html",
-    "./pages/articles/art-4.html",
-    ["articles"],
-  ),
-  createPages(
-    "./src/pages/articles/art-5.html",
-    "./pages/articles/art-5.html",
-    ["articles"],
-  ),
-  createPages("./src/pages/interactives.html", "./pages/interactives.html", [
-    "index",
-  ]),
-
-  createPages("./src/pages/tests/test1.html", "./pages/tests/test1.html", [
-    "test1",
-  ]),
+  createPages("./src/pages/constructor.html", "./pages/constructor.html", ["index"]),
+  createPages("./src/pages/articles/art-1.html", "./pages/articles/art-1.html", ["articles"]),
+  createPages("./src/pages/articles/art-2.html", "./pages/articles/art-2.html", ["articles"]),
+  createPages("./src/pages/articles/art-3.html", "./pages/articles/art-3.html", ["articles"]),
+  createPages("./src/pages/articles/art-4.html", "./pages/articles/art-4.html", ["articles"]),
+  createPages("./src/pages/articles/art-5.html", "./pages/articles/art-5.html", ["articles"]),
+  createPages("./src/pages/articles/art-6.html", "./pages/articles/art-6.html", ["articles"]),
+  createPages("./src/pages/articles/art-7.html", "./pages/articles/art-7.html", ["articles"]),
+  createPages("./src/pages/articles/art-8.html", "./pages/articles/art-8.html", ["articles"]),
+  createPages("./src/pages/articles/art-9.html", "./pages/articles/art-9.html", ["articles"]),
+  createPages("./src/pages/articles/art-10.html", "./pages/articles/art-10.html", ["articles"]),
+  createPages("./src/pages/interactives.html", "./pages/interactives.html", ["index"]),
+  createPages("./src/pages/tests.html", "./pages/tests.html", ["index"]),
+  createPages("./src/pages/tests/test1.html", "./pages/tests/test1.html", ["test1"]),
   createPages("./src/styleguide.html", "./styleguide.html", ["styleguide"]),
 ];
 
