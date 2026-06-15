47a93ae7f23e3054656d25f71086ab9654a9fb02
meenshery <meenshery@gmail.com>
2026-06-15 23:35:27 +0300

    fix 404

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,6 +25,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(".", "docs"),
+    publicPath: "/",
   },
 
   module: {
--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -74,7 +74,7 @@ const htmlPages = [
   createPages("./src/pages/tests/test2.html", "./pages/tests/test2.html", ["allStyles", "puzzle", "headerReact", "footerReact"]),
   createPages("./src/pages/tests/test3.html", "./pages/tests/test3.html", ["allStyles", "test3", "headerReact", "footerReact"]),
   createPages("./src/styleguide.html", "./styleguide.html", ["allStyles"]),
-  createPages("./src/404.html", "./404.html", ["allStyles", "index", "headerReact", "footerReact"]),
+  createPages("./src/404.html", "./404.html", ["allStyles", "headerReact", "footerReact"]),
   createPages("./src/search.html", "./search.html", ["allStyles", "headerReact", "footerReact", "searchReact"]),
 ];
 
